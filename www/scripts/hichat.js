78cab690a93dfe58a9361a6499120ee3849e8332
TinyUlt <TinyUlt@icloud.com>
2015-05-07 02:30:52 +0800

    INIT

---
+++ www/scripts/hichat.js
@@ -0,0 +1,195 @@
+/*
+ *hichat v0.4.2
+ *Wayou Mar 28,2014
+ *MIT license
+ *view on GitHub:https://github.com/wayou/HiChat
+ *see it in action:http://hichat.herokuapp.com/
+ */
+window.onload = function() {
+    var hichat = new HiChat();
+    hichat.init();
+};
+var HiChat = function() {
+    this.socket = null;
+};
+HiChat.prototype = {
+    init: function() {
+        var that = this;
+        this.socket = io.connect();
+        //连接成功
+        this.socket.on('connect', function() {
+            document.getElementById('info').textContent = 'get yourself a nickname :)';
+            document.getElementById('nickWrapper').style.display = 'block';
+            document.getElementById('nicknameInput').focus();
+        });
+        //连接失败
+        this.socket.on('error', function(err) {
+            if (document.getElementById('loginWrapper').style.display == 'none') {
+                document.getElementById('status').textContent = '!fail to connect :(';
+            } else {
+                document.getElementById('info').textContent = '!fail to connect :(';
+            }
+        });
+        //设置登陆按钮事件, 提交事件给服务器
+        document.getElementById('loginBtn').addEventListener('click', function() {
+            var nickName = document.getElementById('nicknameInput').value;
+            if (nickName.trim().length != 0) {
+                that.socket.emit('login', nickName);
+            } else {
+                document.getElementById('nicknameInput').focus();
+            };
+        }, false);
+        //实时监测输入框,如果是回车则登陆
+        document.getElementById('nicknameInput').addEventListener('keyup', function(e) {
+            if (e.keyCode == 13) {
+                var nickName = document.getElementById('nicknameInput').value;
+                if (nickName.trim().length != 0) {
+                    that.socket.emit('login', nickName);
+                };
+            };
+        }, false);
+        //如果有重名, 服务器会发来名字存在消息
+        this.socket.on('nickExisted', function() {
+            document.getElementById('info').textContent = '!nickname is taken, choose another pls';
+        });
+
+        //服务器判断登陆成功, 发送登陆成功消息给客户端
+        this.socket.on('loginSuccess', function() {
+            document.title = 'hichat | ' + document.getElementById('nicknameInput').value;
+            document.getElementById('loginWrapper').style.display = 'none';
+            document.getElementById('messageInput').focus();
+        });
+
+        //系统消息,包括用户进入,用户离开
+        this.socket.on('system', function(nickName, userCount, type) {
+            var msg = nickName + (type == 'login' ? ' joined' : ' left');
+            that._displayNewMsg('system ', msg, 'red');
+            document.getElementById('status').textContent = userCount + (userCount > 1 ? ' users' : ' user') + ' online';
+        });
+        
+        //用户发送的消息
+        this.socket.on('newMsg', function(user, msg, color) {
+            that._displayNewMsg(user, msg, color);
+        });
+        //用户发送的图片
+        this.socket.on('newImg', function(user, img, color) {
+            that._displayImage(user, img, color);
+        });
+        
+        //发送信息按钮回调,发送给服务器和显示到自己的信息窗口
+        document.getElementById('sendBtn').addEventListener('click', function() {
+            var messageInput = document.getElementById('messageInput'),
+                msg = messageInput.value,
+                color = document.getElementById('colorStyle').value;
+            messageInput.value = '';
+            messageInput.focus();
+            if (msg.trim().length != 0) {
+                that.socket.emit('postMsg', msg, color);
+                that._displayNewMsg('me', msg, color);
+                return;
+            };
+        }, false);
+        //实时监测信息输入框,回车键发送
+        document.getElementById('messageInput').addEventListener('keyup', function(e) {
+            var messageInput = document.getElementById('messageInput'),
+                msg = messageInput.value,
+                color = document.getElementById('colorStyle').value;
+            if (e.keyCode == 13 && msg.trim().length != 0) {
+                messageInput.value = '';
+                that.socket.emit('postMsg', msg, color);
+                that._displayNewMsg('me', msg, color);
+            };
+        }, false);
+        //清除信息窗口
+        document.getElementById('clearBtn').addEventListener('click', function() {
+            document.getElementById('historyMsg').innerHTML = '';
+        }, false);
+        document.getElementById('sendImage').addEventListener('change', function() {
+            if (this.files.length != 0) 
+            {
+                var file = this.files[0],
+                    reader = new FileReader(),
+                    color = document.getElementById('colorStyle').value;
+                if (!reader) 
+                {
+                    that._displayNewMsg('system', '!your browser doesn\'t support fileReader', 'red');
+                    this.value = '';
+                    return;
+                };
+                reader.onload = function(e)
+                {
+                    this.value = '';
+                    that.socket.emit('img', e.target.result, color);
+                    that._displayImage('me', e.target.result, color);
+                };
+                reader.readAsDataURL(file);
+            };
+        }, false);
+        this._initialEmoji();
+        document.getElementById('emoji').addEventListener('click', function(e) {
+            var emojiwrapper = document.getElementById('emojiWrapper');
+            emojiwrapper.style.display = 'block';
+            e.stopPropagation();
+        }, false);
+        document.body.addEventListener('click', function(e) {
+            var emojiwrapper = document.getElementById('emojiWrapper');
+            if (e.target != emojiwrapper) {
+                emojiwrapper.style.display = 'none';
+            };
+        });
+        document.getElementById('emojiWrapper').addEventListener('click', function(e) {
+            var target = e.target;
+            if (target.nodeName.toLowerCase() == 'img') {
+                var messageInput = document.getElementById('messageInput');
+                messageInput.focus();
+                messageInput.value = messageInput.value + '[emoji:' + target.title + ']';
+            };
+        }, false);
+    },
+    _initialEmoji: function() {
+        var emojiContainer = document.getElementById('emojiWrapper'),
+            docFragment = document.createDocumentFragment();
+        for (var i = 69; i > 0; i--) {
+            var emojiItem = document.createElement('img');
+            emojiItem.src = '../content/emoji/' + i + '.gif';
+            emojiItem.title = i;
+            docFragment.appendChild(emojiItem);
+        };
+        emojiContainer.appendChild(docFragment);
+    },
+    _displayNewMsg: function(user, msg, color) {
+        var container = document.getElementById('historyMsg'),
+            msgToDisplay = document.createElement('p'),
+            date = new Date().toTimeString().substr(0, 8),
+            //determine whether the msg contains emoji
+            msg = this._showEmoji(msg);
+        msgToDisplay.style.color = color || '#000';
+        msgToDisplay.innerHTML = user + '<span class="timespan">(' + date + '): </span>' + msg;
+        container.appendChild(msgToDisplay);
+        container.scrollTop = container.scrollHeight;
+    },
+    _displayImage: function(user, imgData, color) {
+        var container = document.getElementById('historyMsg'),
+            msgToDisplay = document.createElement('p'),
+            date = new Date().toTimeString().substr(0, 8);
+        msgToDisplay.style.color = color || '#000';
+        msgToDisplay.innerHTML = user + '<span class="timespan">(' + date + '): </span> <br/>' + '<a href="' + imgData + '" target="_blank"><img src="' + imgData + '"/></a>';
+        container.appendChild(msgToDisplay);
+        container.scrollTop = container.scrollHeight;
+    },
+    _showEmoji: function(msg) {
+        var match, result = msg,
+            reg = /\[emoji:\d+\]/g,
+            emojiIndex,
+            totalEmojiNum = document.getElementById('emojiWrapper').children.length;
+        while (match = reg.exec(msg)) {
+            emojiIndex = match[0].slice(7, -1);
+            if (emojiIndex > totalEmojiNum) {
+                result = result.replace(match[0], '[X]');
+            } else {
+                result = result.replace(match[0], '<img class="emoji" src="../content/emoji/' + emojiIndex + '.gif" />');//todo:fix this in chrome it will cause a new request for the image
+            };
+        };
+        return result;
+    }
+};
